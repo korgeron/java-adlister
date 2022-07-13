6723b43d7c5c393b3a9d568bbf8a2650c342607f
orgeron.kevin <orgeron.kevin@gmail.com>
2022-07-13 11:48:50 -0500

    servlet lecture

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String firstName = request.getParameter("fn");
+    String lastName = request.getParameter("ln");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1 style = \"color: red\">Hello " + firstName + " " + lastName + "</h1>");
+
+
+
+
+
+    }
+}
