c3c8ba1b893a53af9d0dc905490711d394fbea07
orgeron.kevin <orgeron.kevin@gmail.com>
2022-07-22 11:06:26 -0500

    push

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,13 +25,18 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         // TODO: find a record in your database that matches the submitted password
+        boolean validAttempt = false;
+        User user = DaoFactory.getUserDao().findByUsername(username);
+        if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+            validAttempt = true;
+        }
         // TODO: make sure we find a user with that username
         // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
+
 
         if (validAttempt) {
             // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
